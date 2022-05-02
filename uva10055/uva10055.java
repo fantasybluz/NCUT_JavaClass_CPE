b5cdaa554bed05a4124ac533789d53244d11b92c
fantasybluz <kira02305141@gmail>
2022-05-03 03:21:50 +0800

    update uva10055.java

---
+++ uva10055/uva10055.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.*;
+public class uva10055 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(Math.abs(a-b));
+    }
+}
