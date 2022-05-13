470a2accb3c59b01d1f109f44392bcf1ac4414a8
dizzysoup <j481936@gamil.com>
2022-05-13 14:33:58 +0800

    uva490_2

--- uva490/uva490.java
+++ uva490/uva490.java
@@ -1,33 +1,28 @@
-import static java.lang.System.out ; 
-import java.util.*;
+import java.util.*; 
 
 public class uva490 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Vector<String> vector = new Vector<String>();
-        int maxlen = 0;
-        int cases = 0;
-        while (sc.hasNextLine()) {
-            cases++;
-            String st = sc.nextLine();
-            vector.add(st);
-            maxlen = Math.max(maxlen, st.length());
+        List<String> list = new ArrayList<String>();
+        int len = 0 ;
+        while(sc.hasNextLine()){
+            String cases = sc.nextLine();
+            if(cases.equals("")) break ; 
+            list.add(cases);
+            len = Math.max(cases.length() , len);
         }
 
-        for (int i = 0; i < maxlen; i++) {
-            int count = 0;
-            for (int j = cases - 1; j >= 0; j--) {
-                if (!(i >= vector.get(j).length())) {
-                    while (count > 0) {
-                        out.print(" ");
-                        count--;
-                    }
-                    out.print(vector.get(j).charAt(i));
-                } else
-                    count++;
-
+        for(int i = 0 ; i< len ; i++){
+            for(int j = list.size()-1 ; j >= 0 ; j--){
+                if( i > list.get(j).length()-1)
+                    if(j == 0 )
+                        System.out.print("");
+                    else 
+                        System.out.print(" ");
+                else
+                System.out.print(list.get(j).charAt(i));
             }
-            out.println();
+            System.out.println();
         }
     }
 
