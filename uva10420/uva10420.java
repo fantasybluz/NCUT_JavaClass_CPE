b320c4a3dc8a4374a10cb7af9cb298e15373227a
dizzysoup <j481936@gamil.com>
2022-05-13 15:01:00 +0800

    uva10420

--- uva10420/uva10420.java
+++ uva10420/uva10420.java
@@ -3,27 +3,27 @@ import java.util.*;
 public class uva10420 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int cases = sc.nextInt();
-        String[] country = new String[cases];
-        Set<String> s = new HashSet<String>();
-
-        for (int i = 0; i < cases; i++) {
-            country[i] = sc.next();
-            s.add(country[i]);
-            String No = sc.nextLine();
-        }
-
+     String[] country = new String[cases];
+        
+        
+        for(int i = 0 ; i< cases ; i++){	
+         country[i] = sc.next();		
+         String nothing = sc.nextLine();
+        }   	
         Arrays.sort(country);
-        String[] arr = s.toArray(new String[s.size()]);
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            int count = 0;
-            for (int j = 0; j < country.length; j++) {
-                if (arr[i].equals(country[j]))
-                    count++;
-            }
-            System.out.println(arr[i] + " " + count);
+               
+       
+        for(int i = 0 ; i< country.length ; i++){
+            int num = 0 ;   		
+            for(int j = i ; j< country.length ; j++){
+                if(country[j].equals(country[i]))
+                    num++;
+                else break;
+            }   		   		
+            System.out.printf("%s %d\r\n",country[i],num);
+            i += (num-1);
         }
 
     }
