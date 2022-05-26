d45faff6fc52046ff5eb8a2826406a6e62bae377
dizzysoup <j481936@gamil.com>
2022-05-26 17:00:52 +0800

    uva10071

---
+++ uva10071/uva10071.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import static java.lang.System.*;
+public class uva10071{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+   	while(sc.hasNext()){
+   		int v = sc.nextInt();
+   		int t = sc.nextInt();
+   		out.println(2*v*t);
+   	}
+    sc.close();
+  }
+};
