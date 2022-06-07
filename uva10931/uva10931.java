b8c877f8ce0fa2a72daec9fe7be9d5a3bafc8a08
dizzysoup <j481936@gamil.com>
2022-06-07 16:50:10 +0800

    uva10931

---
+++ uva10931/uva10931.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class uva10931{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextInt()){
+			int nn = sc.nextInt();
+			if(nn==0)break;
+			String ss = Integer.toBinaryString(nn);
+			char[] cc = ss.toCharArray();
+			int sum = 0;
+			for(int i=0;i<cc.length;i++)
+				if(cc[i]=='1')sum++;
+			System.out.println("The parity of "+ss+" is " + sum + " (mod 2).");
+		}
+        sc.close();
+	}
+}
