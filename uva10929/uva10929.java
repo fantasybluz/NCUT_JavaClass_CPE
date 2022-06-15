bc478ea3cb00996210392c4c3d7470a1e0b37e64
dizzysoup <j481936@gamil.com>
2022-06-15 22:34:04 +0800

    uva10929 fix

--- uva10929/uva10929.java
+++ uva10929/uva10929.java
@@ -1,25 +1,22 @@
 import java.util.*;
-import java.lang.*;
-public class uva10929 {
-    public static void main(String[] args) {
-        Scanner sca = new Scanner(System.in);
-        while(true){
-            String n = sca.next();
-            int odd =0, even =0;
-            if(n.equals(0)){break;}
-            for(int i = 1; i<=n.length(); i++){
-                if(i%2 !=0){
-                    odd += Integer.parseInt(n.substring(i-1, i));
-                }else{
-                    even += Integer.parseInt(n.substring(i-1, i));
-                }
-            }
-            if((odd-even)%11 ==0){
-                System.out.println(n + " is a multiple of 11.");
-            }else{
-                System.out.println(n + " is not a multiple of 11.");
-            }
-
-        }
+import static java.lang.System.*;
+public class uva10929{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    while(sc.hasNext()){
+    	String st = sc.next();
+    	if(st.equals("0")) break ;
+    	int odd = 0 ;
+    	int even = 0 ;
+    	for(int i = 0 ; i < st.length() ; i++){
+    		if ( i %2 == 0 ) even += st.charAt(i) - 48 ;
+    		else odd += st.charAt(i) -48;
+    	}
+    	
+    	if((even - odd ) % 11 == 0 )
+    		out.println( st +  " is a multiple of 11.");
+    	else 
+    		out.println(st + " is not a multiple of 11.");
     }
-}
+  }
+};
