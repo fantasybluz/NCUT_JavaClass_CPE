926996038b9b21d768edd5dcd572e63f170f45c9
dizzysoup <j481936@gamil.com>
2022-06-15 22:22:37 +0800

    uva10055 fix

--- uva10055/uva10055.java
+++ uva10055/uva10055.java
@@ -1,10 +1,25 @@
-import java.util.Scanner;
 import java.util.*;
-public class uva10055 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.print(Math.abs(a-b));
-    }
-}
+import static java.lang.System.*;
+public class uva10055{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+  	while(sc.hasNextLong()){
+  		long n = sc.nextLong();
+  		long m = sc.nextLong();
+  		out.println(Math.abs(n-m));
+  	}
+  }
+};
+ 
+       
+ 
+         
+ 
+  
+ 
+  
+ 
+   
+
+
+ 
