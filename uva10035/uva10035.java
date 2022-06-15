997a11f51305b43f84dca8da277b871740991596
dizzysoup <j481936@gamil.com>
2022-06-15 22:27:53 +0800

    uva10035 fix

--- uva10035/uva10035.java
+++ uva10035/uva10035.java
@@ -1,25 +1,34 @@
 import java.util.*;
-
-public class uva10035 {
-    public static void main(String[] args) {
-        Scanner sca = new Scanner(System.in);
-        int a = sca.nextInt();
-        int b = sca.nextInt();
-        int temp = 0,carry = 0;
-        while (a > 0 || b > 0) {
-            if ((a % 10) + (b % 10) + temp >= 10) {
-                carry++;
-                temp = 1;
-            } else {
-                temp = 1;
-            }
-            a /= 10;
-            b /= 10;
-        }
-        if (carry == 0) {
-            System.out.println("No carry opration.");
-        } else {
-            System.out.println(carry + " carry operation.");
-        }
+import static java.lang.System.*;
+public class uva10035{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    while(sc.hasNextInt()){
+    	int n = sc.nextInt();
+    	int m = sc.nextInt();
+    	int times = 0 ; 
+    	int carry = 0 ;
+    	if(n == 0 && m == 0 ) break ; 
+    	while( n != 0 || m != 0 ){
+    		if(n % 10 + m % 10 + carry >= 10 ){
+    			times ++ ;
+    			carry = 1 ;
+    		}else carry = 0  ;
+    		n /= 10 ;
+    		m /= 10 ;     			
+    	}
+    	switch (times){
+    		case 0 : 
+    			out.println("No carry operation.");
+    			break ; 
+    		case 1 :
+    			out.println(times + " carry operation.");
+    			break ;
+    		default :
+    			out.println(times + " carry operations.");
+    			break;
+    	}
+    	
     }
-}
+  }
+};
